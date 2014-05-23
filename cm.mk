f6e8316c511ab811f4d61e72d344d312ba4f0499
3andala <love3andala@gmail.com>
2014-05-23 20:14:56 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -15,20 +15,14 @@
 # Inherit device configuration
 $(call inherit-product, device/semc/smultron/full_smultron.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := nameless_smultron
+PRODUCT_GMS_CLIENTID_BASE := android-sonyericsson
+PRODUCT_RELEASE_NAME := smultron
+PRODUCT_BRAND := semc
+PRODUCT_MODEL := Xperia Mini
+PRODUCT_MANUFACTURER := SEMC
 
-# Optional CM packages
-PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    VisualizationWallpapers \
-    PhotoTable
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -37,12 +31,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="SEMC/ST15i_1254-2184/ST15i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" \
     PRIVATE_BUILD_DESC="ST15i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
 
-# Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_smultron
-PRODUCT_GMS_CLIENTID_BASE := android-sonyericsson
-
-# Release name
-PRODUCT_RELEASE_NAME := smultron
-
-# Custom tag for unofficial builds
-TARGET_UNOFFICIAL_BUILD_ID := LegacyXperia
